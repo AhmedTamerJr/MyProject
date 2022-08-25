04344e8db8d8cd89e2f9a5d7ecab88eea2d233cb
AhmedTamerJr <at53269@gmail.com>
2022-08-25 13:52:53 +0200

    End of Nav

--- my-app/src/Components/NavBar/NavBar.js
+++ my-app/src/Components/NavBar/NavBar.js
@@ -1,14 +1,21 @@
-import React from 'react'
-
+import React ,{useState}from 'react'
+import  {BiSearch} from "react-icons/bi"
+import {BsSegmentedNav,BsPersonFill} from "react-icons/bs"
+import {TiSocialFacebook ,TiSocialTwitter,TiSocialYoutubeCircular ,TiSocialGithub ,TiSocialInstagramCircular} from "react-icons/ti"
+import "./NavBar.css"
 export default function NavBar() {
+
+    const [nav, setNav]=useState(false)
+    const handleNav=()=> setNav(!nav)
+    
   return (
     
-    <div className='NavBar'>
+    <div className='NavBar '>
         <div>
             <h2>BEACH.</h2>
         </div>
-        
-        <ul>
+
+        <ul className='Nav-Menu'>
             <li>Home</li>
             <li>Destination</li>
             <li>Travel</li>
@@ -16,9 +23,41 @@ export default function NavBar() {
             <li>Views</li>
         </ul>
 
-        <div className='Nav-Icons'>
-
+        <div className='Nav-Icons'>   
+        <BiSearch className="icon"/>
+        <BsPersonFill className="icon"/>
         </div>
+
+        <div className='Right-Side'>
+            <BsSegmentedNav className="icon" onClick={handleNav}/>
+        </div>
+        {/* mobile menu */}
+        <div className={nav ? 'Mobile-Menu active':"Mobile-Menu"}> 
+            <ul className='mobile-nav'>
+                <li>Home</li>
+                <li>Destination</li>
+                <li>Travel</li>
+                <li>Book</li>
+                <li>Views</li>
+            </ul>
+
+            <div className='Mobile-menu-bottom'>
+                
+                <div className='menu-Buttons'>
+                    <button>Search</button>
+                    <button>Account</button>
+                </div>
+
+                <div className='Social-Media'>
+                    <TiSocialFacebook className="icon"/>
+                    <TiSocialTwitter className="icon"/>
+                    <TiSocialInstagramCircular className="icon"/>
+                    <TiSocialYoutubeCircular className="icon"/>
+                    <TiSocialGithub className="icon"/>
+                </div>
+            </div>
+        </div>
+        {/* End on mobile menu */}
 
     </div>
 
