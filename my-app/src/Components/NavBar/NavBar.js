76922cbad0126d40c05515c883898e770b0845d0
AhmedTamerJr <at53269@gmail.com>
2022-08-25 10:15:55 +0200

    Adding the feature for icons

---
+++ my-app/src/Components/NavBar/NavBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    
+    <div className='NavBar'>
+        <div>
+            <h2>BEACH.</h2>
+        </div>
+        
+        <ul>
+            <li>Home</li>
+            <li>Destination</li>
+            <li>Travel</li>
+            <li>Book</li>
+            <li>Views</li>
+        </ul>
+
+        <div className='Nav-Icons'>
+
+        </div>
+
+    </div>
+
+  )
+}
